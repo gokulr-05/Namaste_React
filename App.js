764a51ec7c83d5b501b2ed758f96e28ea6507661
Gokul R <gokulravivcr05@gmail.com>
2025-09-26 07:46:10 +0530

    added styles css file

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const heading = React.createElement(
   'h1',
-  { id: 'heading 1', xyz: 'abc' },
+  { id: 'heading1', xyz: 'abc' },
   'Hello World from React!',
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
