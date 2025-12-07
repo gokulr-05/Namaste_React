a384b285fe122a510d2c7f09df290e46a888e7d5
Gokul <gokulravivcr05@gmail.com>
2025-12-07 19:33:55 +0530

    worked on dependencies

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading1", xyz: "abc" },
@@ -27,7 +30,7 @@ const heading = React.createElement(
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h1", {}, "I'm h1111 tag"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
